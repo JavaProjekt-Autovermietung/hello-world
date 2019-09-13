0307c89a39bd8562c56b5bf6aed74ebeafdf2a97
JavaProjekt-Autovermietung <pimpthegalaxy@gmail.com>
2019-09-13 13:15:34 +0200

    Klasse Fahrzeug wird erzeut

---
+++ Autovermietung/src/Autovermietung/Fahrzeug.java
@@ -0,0 +1,18 @@
+package Autovermietung;
+
+public class Fahrzeug {
+	
+	enum Preisklassen {A,B,C};
+	
+	private String Fahrzeugnummer;
+	private String Fahrzeugtyp;
+	private Preisklassen Preisklasse;
+	private double kmStandVorher, kmStandNacher;
+	private double Verbrauch;
+	private boolean Wartung;
+	private boolean Reparatur;
+	
+	
+	
+
+}
