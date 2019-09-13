cd352247d126e5dfd2eb85f0a1252054ed38aa94
JavaProjekt-Autovermietung <pimpthegalaxy@gmail.com>
2019-09-13 10:54:08 +0200

    Initial Project Creation

---
+++ Autovermietung/src/Autovermietung/Haupt.java
@@ -0,0 +1,12 @@
+package Autovermietung;
+
+public class Haupt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+		
+	}
+
+}
