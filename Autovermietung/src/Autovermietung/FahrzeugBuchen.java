1f0d49f23e9e4729188f4815b567f09f4693a307
JavaProjekt-Autovermietung <pimpthegalaxy@gmail.com>
2019-09-23 08:30:36 +0200

    ddd

--- Autovermietung/src/Autovermietung/FahrzeugBuchen.java
+++ Autovermietung/src/Autovermietung/FahrzeugBuchen.java
@@ -10,37 +10,62 @@ import java.awt.Font;
 import javax.swing.JLabel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JButton;
 
-public class FahrzeugPanel extends JPanel {
+public class FahrzeugBuchen extends JPanel {
 
 	/**
 	 * Create the panel.
 	 */
-	public FahrzeugPanel() {
+	public FahrzeugBuchen() {
 		setLayout(null);
 		
 		JLabel lblFahrzeug = new JLabel("Fahrzeug");
-		lblFahrzeug.setBounds(169, 45, 104, 29);
+		lblFahrzeug.setBounds(169, 40, 104, 29);
 		lblFahrzeug.setFont(new Font("Tahoma", Font.PLAIN, 24));
 		add(lblFahrzeug);
 		
-		DatePicker datePicker = new DatePicker();
-		datePicker.getComponentToggleCalendarButton().addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		datePicker.setBounds(68, 100, 139, 20);
-		add(datePicker);
-		datePicker.setDateToToday();
-		
 		DateTimePicker dateTimePicker = new DateTimePicker();
-		dateTimePicker.setBounds(68, 192, 217, 23);
+		dateTimePicker.setBounds(72, 173, 217, 23);
 		add(dateTimePicker);
 		dateTimePicker.datePicker.setDateToToday();
 		dateTimePicker.timePicker.setTimeToNow();
+		
+		JLabel lblNewLabel = new JLabel("Kombi / VW / Golf / B-LNK4 / Mittelklasse");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		lblNewLabel.setBounds(72, 79, 244, 20);
+		add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Anfangsdatum:");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		lblNewLabel_1.setBounds(72, 110, 104, 13);
+		add(lblNewLabel_1);
+		
+		JLabel lblZurckAm = new JLabel("Enddatum:");
+		lblZurckAm.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		lblZurckAm.setBounds(71, 158, 104, 13);
+		add(lblZurckAm);
+		
+		JButton btnBuchen = new JButton("Buchen");
+		btnBuchen.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		btnBuchen.setBounds(71, 235, 90, 21);
+		add(btnBuchen);
+		
+		JButton btnReservieren = new JButton("Reservieren");
+		btnReservieren.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		btnReservieren.setBounds(169, 235, 90, 21);
+		add(btnReservieren);
+		
+		JButton btnWartung = new JButton("Wartung");
+		btnWartung.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		btnWartung.setBounds(269, 235, 90, 21);
+		add(btnWartung);
+		
+		DateTimePicker dateTimePicker_1 = new DateTimePicker();
+		dateTimePicker_1.setBounds(71, 125, 217, 23);
+		add(dateTimePicker_1);
 
 		
 
 	}
-
 }
